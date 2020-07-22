c590f589e3695283d807ac537de651b3bf98a3b6
parvathys0311 <parvathys0311@gmail.com>
2020-07-22 16:57:20 -0600

    added current to nav

---
+++ public/js/nav.js
@@ -0,0 +1,23 @@
+// const btn = document.querySelector('nav a');
+// console.log(btn);
+
+// btn.addEventListener('click', function () {
+//   btn.classList.toggle('current');
+
+// });
+
+let urlString = document.location.href;
+let urlArray = urlString.split('/');
+let currentURL = urlArray[urlArray.length-1];
+console.log(currentURL);
+
+let links = document.querySelectorAll('nav li a')
+// console.log(links);
+
+links.forEach(function (item) {
+  let pageUrl = item.href.split("/");
+  console.log(pageUrl);
+  if(pageUrl[pageUrl.length-1] == currentURL) {
+    item.className = "current";
+  }
+});
